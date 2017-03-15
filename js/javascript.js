3b5d88d1a7ed25a5364ff5f77e935ae02e3888cf
Student <idk123@gmail.com>
2017-03-15 09:11:21 -1000

    modified:   js/javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,46 @@
-var jsonDataBody = [
-	{
-		"header" : [
-		"h1",
-		"h4",
-		"hr"
-	}
-	{
-		"div" : {
-			"class" : "wrapper",
-			["h2","hr","nav"]
-		}
-	}
-];
+// var jsonDataBody = [
+// 	{
+// 		"header" : [
+// 		"h1",
+// 		"h4",
+// 		"hr"
+// 	}
+// 	{
+// 		"div" : {
+// 			"class" : "wrapper",
+// 			["h2","hr","nav"]
+// 		}
+// 	}
+// ];
 
-var entryJson = [
+// var entryJson = [
+// 	{
+// 		"div" : {
+// 			"id" : "fith",
+// 			["h1" : {
+// 				"innerHTML" : "Purpose"
+// 			} 
+// 			{"p": 
+// 			  {
+// 				"innterHTML": "What I thought of CSS initially was, it was really confusing, and really difficult and I would take years to understand what I'm doing with it."
+// 				}
+// 			}
+// 			]
+// 		}
+// 	}
+// ]
+
+var simplifiedJSON = [
 	{
-		"div" : {
-			"id" : "fith",
-			["h1", "p"]
-		}
+		"journalEntry" : "1"
+		"date": "Aug 15",
+		"title": "My first entry",
+		"content": "I want to learn how to create video games, and I was really interested in coding. I wanna read programming langages easily, and create things with those languages."
+	}
+	{
+		"journalEntry" : "2"
+		"date" : "24 Aug, 2016",
+		"title": "UX vs. UI",
+		"content" : "Difference Between UX and UI. UX stands for user experience and focuses on how the user is experiencing a site. UI stands for user interface and focuses on what the user sees."
 	}
 ]
