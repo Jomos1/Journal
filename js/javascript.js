66fef7c4dd976c0dcaff4d7f02f380105754ae5a
Student <idk123@gmail.com>
2017-03-15 08:37:11 -1000

    deleted:    javascript.js 	new file:   js/javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+var jsonDataBody = [
+	{
+		"header" : [
+		"h1",
+		"h4",
+		"hr"
+	}
+	{
+		"div" : {
+			"class" : "wrapper",
+			["h2","hr","nav"]
+		}
+	}
+];
+
+var entryJson = [
+	{
+		"div" : {
+			"id" : "fith",
+			["h1", "p"]
+		}
+	}
+]
